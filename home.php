a404382cf776c86d5195eef79b8a2d1c192f6594
andrevmog <andre.machado@zeeng.com.br>
2021-09-15 19:45:42 -0300

    mudanças

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/png" href="img/wow.ico">
+    <link rel="icon" type="image/png" href="img/wow.ico">
 </head>
 
 <body>
