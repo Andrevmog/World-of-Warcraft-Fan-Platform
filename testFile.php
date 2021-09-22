50d82ac2324f392a8172b5ba6c780c206b9d6766
Vitor Galante <vccastro.galante@gmail.com>
2021-09-22 10:32:41 -0300

    test.php Name Change

--- testFile.php
+++ testFile.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../WoWFanSiteCredentials/credentials.php'; // Secret file with developer info
+require_once '../../WoWFanSiteCredentials/credentials.php'; // Secret file with developer info
 
 $authorization = new OAtuth(); // Ignore the error on the editor
 $acessToken = $authorization->generateAcessToken(); // Calls the function to generate a Auth token
@@ -12,6 +12,7 @@ print_r($acessToken); // Info about the generated token
 $acessToken = $acessToken['raw_response']['access_token']; // Extracts only the numerical token used to make requests
 
 // Test Function to see if it works
+/* 
 function test() {
     global $acessToken; // Use to acess the acessToken variable inside this function
 
@@ -34,6 +35,28 @@ function test() {
 
     return $accessTokenResponse;
 }
-
 echo '<pre>';
 print_r(test()); // Info about the Wow token
+*/
+
+function mountsIndex() {
+    global $acessToken; // Use to acess the acessToken variable inside this function
+
+    // Header as battle.net documentation
+    $acessTokenHeader = [
+        "Authorization: Bearer " . $acessToken
+    ];
+
+    $ch = curl_init();
+    $url = 'https://us.api.blizzard.com/data/wow/mount/index?namespace=static-us&locale=en_US'; // Index of all mounts in WoW
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $acessTokenHeader);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER, false);
+
+    $response = curl_exec($ch); // Receives the response from the server
+    curl_close($ch);
+
+    $accessTokenResponse = json_decode($response, true); // Decodes the response as an array
+    return $accessTokenResponse;
+}
